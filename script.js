8f980cd2ee66542d56a87895209f6b22136aa1e9
Lorena Cardoso <lollycardoso@eaportal.org>
2024-03-27 22:39:05 -0300

    quarto commit do JS

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ function receberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao =>{
-    botao.addEventListener("click", receberPorcentagensBotao )
+    botao.addEventListener("click", receberPorcentagens )
 })
 
-function receberPorcentagensBotao(evento) {
+function receberPorcentagens(evento) {
     botoesGorjeta.forEach(botao =>{
         botao.classList.remove("botao-ativo")
 
@@ -40,5 +40,15 @@ function receberPorcentagensBotao(evento) {
             botao.classList.add("botao-ativo")
         }
     })
-    porcentagem = parseFloat(evento.target.value)/100
+
+    if(evento.target.value !== "") {
+        porcentagem = parseFloat(evento.target.value)/100
+    } else {
+        porcentagem = 0
+    }
+
+    console.log(porcentagem)
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagens)
